cef5725e4c0dd8d2b719ad732186c2768e657e98
hafeezatb <hafeezat.bishi73@journalism.cuny.edu>
2022-03-01 20:21:45 -0500

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-/**
- * SILENCE IS GOLDEN
- */
+const body = document.querySelector("body");
+const lightswitches = document.querySelectorAll(".light-switch");
+
+lightswitches.forEach(function(lightswitch) {
+    lightswitch.addEventListener("click", function (e) {e.preventDefault();
+
+        body.classList.toggle("light-off");
+    });
+    
+
+});
+
